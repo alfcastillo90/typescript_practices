9fa3020f2f9849820bfbd36e4a257d13bbfa6380
alfcastillo90 <cacr1990@gmail.com>
2020-12-01 10:12:47 -0300

    Add: algunos ejercicios

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    ;
+    const enviarMision = (xmen) => {
+        console.log(`Enviando a ${xmen.nombre} a la misión`);
+    };
+    const regresarAlCuartel = (xmen) => {
+        console.log(`Enviando a ${xmen.nombre} a la misión`);
+    };
+    const wolwerine = {
+        nombre: 'Logan',
+        edad: 60,
+    };
+    enviarMision(wolwerine);
+    regresarAlCuartel(wolwerine);
+})();
